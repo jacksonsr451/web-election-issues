da07c0d2863996d4901d97d457136aeac458b8cf
Jackson Severino da Rocha <jackson.severino.rocha@escola.pr.gov.br>
2024-07-17 09:32:57 -0300

    update values to server

--- src/services/server.ts
+++ src/services/server.ts
@@ -3,7 +3,7 @@ import { jwtDecode } from 'jwt-decode'
 import { useUserAppStore } from '@/stores/user'
 import { useAuthAppStore } from '@/stores/auth'
 
-const baseURI = 'http://api-election-issues'
+const baseURI = 'http://195.200.1.150:8080'
 
 interface DecodedToken {
   sub: string
