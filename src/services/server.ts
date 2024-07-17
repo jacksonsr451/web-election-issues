2c5e41e170487a8363c67b3448b4e63be8c3902d
Jackson Severino da Rocha <jackson.severino.rocha@escola.pr.gov.br>
2024-07-17 13:08:03 -0300

    include ssl

--- src/services/server.ts
+++ src/services/server.ts
@@ -3,7 +3,7 @@ import { jwtDecode } from 'jwt-decode'
 import { useUserAppStore } from '@/stores/user'
 import { useAuthAppStore } from '@/stores/auth'
 
-const baseURI = 'http://dataprevia.com.br:8000'
+const baseURI = 'http://195.200.1.150:8000'
 
 interface DecodedToken {
   sub: string
